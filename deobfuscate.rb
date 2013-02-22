01580095cfcda1e1e29a1e4f57b369148be4d9a7
Rotem Rubnov <rotem.rubnov@100grams.nl>
2013-02-22 14:25:32 +0100

    now adding/removing deobfuscation macro in ruby scripts

--- deobfuscate.rb
+++ deobfuscate.rb
@@ -2,13 +2,20 @@ require 'Base64'
 
 defines_file = 'defines.h'
 
+def remove_deobfuscate_macro(lines)
+  index = lines.gsub!(/(\n#define\s+deobfuscate\(exp\).+)\n/, '')
+end
+
+
 # deobfuscate in defines_file
 
 lines = IO.readlines(defines_file).join
 target = ""
 count = 0
+has_deobfuscate_macro = false
 lines.each do |line| 
   if line =~/#define\s/ 
+    define_name = line.scan(/#define\s+(\w+)/).flatten
     enc_text = line.scan(/#define\s+\w+\s+deobfuscate\((.*)\)/).flatten.first
     if enc_text != nil && enc_text.length > 0
       enc_text += "\n"
@@ -16,12 +23,14 @@ lines.each do |line|
       line.gsub!(/(#define\s+\w+\s+)(.+)/, "\\1#{clear_text}")
       count +=1
     end
+    has_deobfuscate_macro |= define_name.first =~ /deobfuscate/
   end
   target << line
 end
 
 if count > 0
   puts "deobfuscated #{count} strings in #{defines_file}"
+  remove_deobfuscate_macro(target) if has_deobfuscate_macro
   File.open(defines_file, 'w') {|f| f.write(target)}
 else 
   puts "no obfuscated strings found in #{defines_file}"
--- obfuscate.rb
+++ obfuscate.rb
@@ -2,27 +2,37 @@ require 'Base64'
 
 defines_file = 'defines.h'
 
+def add_deobfuscate_macro(lines)
+  index = lines.rindex(/(\s+)#endif/)
+  lines.insert(index, "\n#define deobfuscate(exp) \[NSString stringWithBase64EncodedString:\[NSString stringWithFormat:\@\"%@\\n\", exp]]\n")
+end
+
+
 # obfuscate string literals in defines_file
 
 lines = IO.readlines(defines_file).join
+has_deobfuscate_macro = false
 target = ""
 count = 0
 lines.each do |line| 
   if line =~/#define\s/ 
     clear_text = line.scan(/#define\s+\w+\s+(.*)/).flatten
-    if clear_text.first !=nil && clear_text.first.length > 0
+    define_name = line.scan(/#define\s+(\w+)/).flatten
+    if define_name.first !=~ /deobfuscate/ && clear_text.first !=nil && clear_text.first.length > 0
       break if clear_text.first =~ /deobfuscate/
       enc_text = Base64.encode64(clear_text.first)
       enc_text.delete! "\n"
       line.gsub!(/(#define\s+\w+\s+)(.+)/, "\\1deobfuscate(#{enc_text})")
-      count +=1
+      count +=1      
     end
+    has_deobfuscate_macro |= define_name.first =~ /deobfuscate/
   end
   target << line
 end
 
 if count > 0
   puts "obfuscated #{count} strings in #{defines_file}"
+  add_deobfuscate_macro(target) if !has_deobfuscate_macro
   File.open(defines_file, 'w') {|f| f.write(target)}
 else 
   puts "no new strings to obfuscate in #{defines_file}"
